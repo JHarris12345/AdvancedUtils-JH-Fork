114a9d83c7216b5094594511255b358700339dc7
GC-spigot <lietuvis.travian@gmail.com>
2023-04-10 16:14:27 +0300

    Version bump

--- src/main/java/net/advancedplugins/utils/Registry.java
+++ src/main/java/net/advancedplugins/utils/Registry.java
@@ -3,10 +3,7 @@ package net.advancedplugins.utils;
 public class Registry {
 
     public static String get() {
-        if(!"%%__USER__%%".equals("%%__USER__%%"))
-            return null;
-
-        return "9454";
-//        return "%%__USER__%%";
+//        return "9454";
+        return "%%__USER__%%";
     }
 }
