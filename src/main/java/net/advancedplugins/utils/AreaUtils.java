0ffa4a19ea706df245d2b7b3fcd3dc14a56c67d6
ThomasWega <tomous.wega@gmail.com>
2023-11-07 17:17:51 +0100

    improved AreaUtils + fixed Aoe target

--- src/main/java/net/advancedplugins/utils/AreaUtils.java
+++ src/main/java/net/advancedplugins/utils/AreaUtils.java
@@ -16,6 +16,7 @@ public class AreaUtils {
 
     // AOE accessibility application controller
     // done for AE version 3.0.0 release (to be concluded)
+    // OLD IMPLEMENTATION
     public static List<LivingEntity> getPlayersInRadius(int radius, Entity p, boolean checkForDamage, boolean damageable, boolean mobs) {
         List<LivingEntity> playersList = new ArrayList<>();
 
@@ -59,4 +60,63 @@ public class AreaUtils {
         return playersList;
     }
 
+    public static List<LivingEntity> getPlayersInRadius(int radius, Entity p, RadiusTarget target) {
+        List<LivingEntity> playersList = new ArrayList<>();
+
+        switch (target) {
+            case ALL:
+                for (Entity entity : p.getNearbyEntities(radius, radius, radius)) {
+                    if (entity instanceof LivingEntity) {
+                        playersList.add((LivingEntity) entity);
+                    }
+                }
+                break;
+            case PLAYERS:
+                for (Entity entity : p.getNearbyEntities(radius, radius, radius)) {
+                    if (entity instanceof Player) {
+                        playersList.add((Player) entity);
+                    }
+                }
+                break;
+            case MOBS:
+                for (Entity entity : p.getNearbyEntities(radius, radius, radius)) {
+                    if (entity instanceof LivingEntity && !(entity instanceof Player) && !(entity instanceof ArmorStand)) {
+                        playersList.add((LivingEntity) entity);
+                    }
+                }
+                break;
+            case DAMAGEABLE:
+                for (Entity entity : p.getNearbyEntities(radius, radius, radius)) {
+                    if (entity instanceof LivingEntity && isDamageable(entity)) {
+                        playersList.add((LivingEntity) entity);
+                    }
+                }
+                break;
+            case UNDAMAGEABLE:
+                for (Entity entity : p.getNearbyEntities(radius, radius, radius)) {
+                    if (entity instanceof LivingEntity && !isDamageable(entity)) {
+                        playersList.add((LivingEntity) entity);
+                    }
+                }
+                break;
+        }
+
+        return playersList;
+    }
+
+    private static boolean isDamageable(Entity entity) {
+        entity.setMetadata("ae_ignore", new FixedMetadataValue(ASManager.getInstance(), true));
+        EntityDamageByEntityEvent event = new EntityDamageByEntityEvent(entity, entity, EntityDamageEvent.DamageCause.CUSTOM, 0);
+        Bukkit.getPluginManager().callEvent(event);
+        entity.removeMetadata("ae_ignore", ASManager.getInstance());
+        return !event.isCancelled();
+    }
+
+    public enum RadiusTarget {
+        ALL,
+        PLAYERS,
+        MOBS,
+        DAMAGEABLE,
+        UNDAMAGEABLE
+    }
 }
