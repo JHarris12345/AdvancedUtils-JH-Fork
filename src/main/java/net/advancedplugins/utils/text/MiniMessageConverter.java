e55ddfcd3d7711ff481b760e8bb3e3a5a9dd8a63
Wega <tomous.wega@gmail.com>
2023-11-18 16:18:05 +0100

    toast messages + papi + minimessage

---
+++ src/main/java/net/advancedplugins/utils/text/MiniMessageConverter.java
@@ -0,0 +1,49 @@
+package net.advancedplugins.utils.text;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MiniMessageConverter {
+
+    // Mapping of Minecraft color codes to MiniMessage tags
+    private static final Map<String, String> colorMap = new HashMap<>();
+
+    static {
+        colorMap.put("&0", "<black>");
+        colorMap.put("&1", "<dark_blue>");
+        colorMap.put("&2", "<dark_green>");
+        colorMap.put("&3", "<dark_aqua>");
+        colorMap.put("&4", "<dark_red>");
+        colorMap.put("&5", "<dark_purple>");
+        colorMap.put("&6", "<gold>");
+        colorMap.put("&7", "<gray>");
+        colorMap.put("&8", "<dark_gray>");
+        colorMap.put("&9", "<blue>");
+        colorMap.put("&a", "<green>");
+        colorMap.put("&b", "<aqua>");
+        colorMap.put("&c", "<red>");
+        colorMap.put("&d", "<light_purple>");
+        colorMap.put("&e", "<yellow>");
+        colorMap.put("&f", "<white>");
+        colorMap.put("&l", "<bold>");
+        colorMap.put("&m", "<strikethrough>");
+        colorMap.put("&n", "<underlined>");
+        colorMap.put("&o", "<italic>");
+        colorMap.put("&r", "<reset>");
+    }
+
+    public static String convertLegacy(@NotNull String legacyString) {
+        for (Map.Entry<String, String> entry : colorMap.entrySet()) {
+            legacyString = legacyString.replace(entry.getKey(), entry.getValue());
+        }
+        return legacyString;
+    }
+
+    public static Component getFromLegacy(@NotNull String legacyString) {
+        return MiniMessage.miniMessage().deserialize(convertLegacy(legacyString));
+    }
+}
