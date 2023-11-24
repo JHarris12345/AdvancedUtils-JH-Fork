db0d3f23d945719af9d35bab126a3d4b3d864280
Wega <tomous.wega@gmail.com>
2023-11-24 15:38:03 +0100

    oraxen support for telepathy + trench dupe fix

--- src/main/java/net/advancedplugins/utils/hooks/plugins/OraxenHook.java
+++ src/main/java/net/advancedplugins/utils/hooks/plugins/OraxenHook.java
@@ -1,12 +1,27 @@
 package net.advancedplugins.utils.hooks.plugins;
 
 import io.th0rgal.oraxen.api.OraxenBlocks;
+import io.th0rgal.oraxen.api.events.noteblock.OraxenNoteBlockBreakEvent;
+import io.th0rgal.oraxen.mechanics.provided.gameplay.noteblock.NoteBlockMechanic;
+import io.th0rgal.oraxen.mechanics.provided.gameplay.stringblock.StringBlockMechanic;
+import io.th0rgal.oraxen.utils.drops.Loot;
+import net.advancedplugins.utils.ASManager;
 import net.advancedplugins.utils.hooks.HookPlugin;
 import net.advancedplugins.utils.hooks.PluginHookInstance;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
 
 public class OraxenHook extends PluginHookInstance implements Listener {
+    private final Plugin plugin;
+
+    public OraxenHook(Plugin plugin) {
+        this.plugin = plugin;
+    }
 
     @Override
     public boolean isEnabled() {
@@ -20,5 +35,42 @@ public class OraxenHook extends PluginHookInstance implements Listener {
 
     public boolean isCustomBlock(Block block) {
         return OraxenBlocks.isOraxenBlock(block);
+    }
+
+    public boolean isCustomStringBlock(Block block) {
+        return OraxenBlocks.isOraxenStringBlock(block);
+    }
+
+    public boolean isCustomNoteBlock(Block block) {
+        return OraxenBlocks.isOraxenNoteBlock(block);
+    }
+
+    @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
+    public void onCustomBlockBreak(OraxenNoteBlockBreakEvent event) {
+        Player player = event.getPlayer();
+        Block block = event.getBlock();
+        // otherwise the item is put in the inventory and at the same time is
+        if (block.hasMetadata("telepathy-broken-oraxen")) {
+            block.removeMetadata("telepathy-broken-oraxen", plugin);
+            event.setCancelled(true);
+
+            ItemStack[] loot = new ItemStack[0];
+            if (isCustomNoteBlock(block)) {
+                NoteBlockMechanic customBlock = OraxenBlocks.getNoteBlockMechanic(block.getBlockData());
+                loot = customBlock.getDrop().getLoots().stream()
+                        .map(Loot::getItemStack)
+                        .toArray(ItemStack[]::new);
+            } else if (isCustomStringBlock(block)) {
+                StringBlockMechanic customBlock = OraxenBlocks.getStringMechanic(block.getBlockData());
+                loot = customBlock.getDrop().getLoots().stream()
+                        .map(Loot::getItemStack)
+                        .toArray(ItemStack[]::new);
+            }
+
+            // if player is not null, the remove method will drop item automatically
+            if (OraxenBlocks.remove(block.getLocation(), null)) {
+                ASManager.giveItem(player, loot);
+            }
+        }
     }
 }
