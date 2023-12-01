0d7d00b566ee639a076d4196b54a078fe60e740a
GC <lietuvis.travian@gmail.com>
2023-12-01 22:25:18 +0200

    AOE target now ignores entities that are not alive (e.g. paintings)

--- src/main/java/net/advancedplugins/utils/AreaUtils.java
+++ src/main/java/net/advancedplugins/utils/AreaUtils.java
@@ -1,30 +1,37 @@
 package net.advancedplugins.utils;
 
 import org.bukkit.Bukkit;
-import org.bukkit.entity.ArmorStand;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.*;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.metadata.FixedMetadataValue;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class AreaUtils {
 
-    // AOE accessibility application controller
+    private static final List<EntityType> ignoredEntities = Collections.unmodifiableList(Arrays.asList(EntityType.ARMOR_STAND,
+            EntityType.PLAYER, EntityType.ITEM_FRAME, EntityType.PAINTING, EntityType.LEASH_HITCH, EntityType.MINECART,
+            EntityType.MINECART_CHEST, EntityType.MINECART_COMMAND, EntityType.MINECART_FURNACE, EntityType.MINECART_HOPPER,
+            EntityType.MINECART_MOB_SPAWNER, EntityType.MINECART_TNT, EntityType.BOAT, EntityType.FISHING_HOOK,
+            EntityType.DROPPED_ITEM, EntityType.ARROW, EntityType.SPECTRAL_ARROW, EntityType.EGG, EntityType.ENDER_PEARL,
+            EntityType.ENDER_SIGNAL, EntityType.EXPERIENCE_ORB, EntityType.FIREBALL, EntityType.FIREWORK,
+            EntityType.SMALL_FIREBALL, EntityType.LLAMA_SPIT, EntityType.SNOWBALL, EntityType.SPLASH_POTION,
+            EntityType.THROWN_EXP_BOTTLE, EntityType.WITHER_SKULL, EntityType.SHULKER_BULLET, EntityType.PRIMED_TNT,
+            EntityType.TRIDENT, EntityType.DRAGON_FIREBALL, EntityType.LIGHTNING, EntityType.AREA_EFFECT_CLOUD, EntityType.UNKNOWN));
+
     // done for AE version 3.0.0 release (to be concluded)
     // OLD IMPLEMENTATION
-    public static List<LivingEntity> getPlayersInRadius(int radius, Entity p, boolean checkForDamage, boolean damageable, boolean mobs) {
+    public static List<LivingEntity> getEntitiesInRadius(int radius, Entity p, boolean checkForDamage, boolean damageable, boolean mobs) {
         List<LivingEntity> playersList = new ArrayList<>();
 
         // Check for players only.
         if (!mobs) {
             for (Entity entity : p.getNearbyEntities(radius, radius, radius)) {
-                if (!(entity instanceof Player))
-                    continue;
+                if (!(entity instanceof Player)) continue;
                 Player target = (Player) entity;
                 if (checkForDamage) {
                     target.setMetadata("ae_ignore", new FixedMetadataValue(ASManager.getInstance(), true));
@@ -48,25 +55,22 @@ public class AreaUtils {
             // Check for mobs only.
         } else {
             for (Entity ent : p.getNearbyEntities(radius, radius, radius)) {
-                if (!(ent instanceof LivingEntity))
-                    continue;
-                if (ent instanceof Player)
-                    continue;
-                if (ent instanceof ArmorStand)
-                    continue;
+                if (!(ent instanceof LivingEntity)) continue;
+                if (ent instanceof Player) continue;
+                if (ent instanceof ArmorStand) continue;
                 playersList.add((LivingEntity) ent);
             }
         }
         return playersList;
     }
 
-    public static List<LivingEntity> getPlayersInRadius(int radius, Entity p, RadiusTarget target) {
+    public static List<LivingEntity> getEntitiesInRadius(int radius, Entity p, RadiusTarget target) {
         List<LivingEntity> playersList = new ArrayList<>();
 
         switch (target) {
             case ALL:
                 for (Entity entity : p.getNearbyEntities(radius, radius, radius)) {
-                    if (entity instanceof LivingEntity) {
+                    if (entity instanceof LivingEntity && !ignoredEntities.contains(entity.getType())) {
                         playersList.add((LivingEntity) entity);
                     }
                 }
@@ -80,21 +84,22 @@ public class AreaUtils {
                 break;
             case MOBS:
                 for (Entity entity : p.getNearbyEntities(radius, radius, radius)) {
-                    if (entity instanceof LivingEntity && isDamageable(entity) && !(entity instanceof Player) && !(entity instanceof ArmorStand)) {
+                    if (entity instanceof LivingEntity && isDamageable(entity)
+                            && !ignoredEntities.contains(entity.getType()) && !(entity instanceof Player)) {
                         playersList.add((LivingEntity) entity);
                     }
                 }
                 break;
             case DAMAGEABLE:
                 for (Entity entity : p.getNearbyEntities(radius, radius, radius)) {
-                    if (entity instanceof LivingEntity && isDamageable(entity)) {
+                    if (entity instanceof LivingEntity && isDamageable(entity) && !ignoredEntities.contains(entity.getType())) {
                         playersList.add((LivingEntity) entity);
                     }
                 }
                 break;
             case UNDAMAGEABLE:
                 for (Entity entity : p.getNearbyEntities(radius, radius, radius)) {
-                    if (entity instanceof LivingEntity && !isDamageable(entity)) {
+                    if (entity instanceof LivingEntity && !isDamageable(entity) && !ignoredEntities.contains(entity.getType())) {
                         playersList.add((LivingEntity) entity);
                     }
                 }
@@ -113,10 +118,6 @@ public class AreaUtils {
     }
 
     public enum RadiusTarget {
-        ALL,
-        PLAYERS,
-        MOBS,
-        DAMAGEABLE,
-        UNDAMAGEABLE
+        ALL, PLAYERS, MOBS, DAMAGEABLE, UNDAMAGEABLE
     }
 }
