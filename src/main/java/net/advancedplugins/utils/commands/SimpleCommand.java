35d73bb8e8b594fba2f14f1bc1d6528727d611c2
GC <lietuvis.travian@gmail.com>
2023-12-15 23:40:31 +0200

    Added single commands to help menu

--- src/main/java/net/advancedplugins/utils/commands/SimpleCommand.java
+++ src/main/java/net/advancedplugins/utils/commands/SimpleCommand.java
@@ -1,9 +1,8 @@
 package net.advancedplugins.utils.commands;
 
-import com.google.common.collect.Sets;
+import lombok.AllArgsConstructor;
 import net.advancedplugins.utils.text.Text;
 import org.apache.commons.lang.StringUtils;
-import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
@@ -11,7 +10,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.Set;
 
 public abstract class SimpleCommand<T extends CommandSender> extends Command<T> {
     private final String command;
@@ -20,6 +18,7 @@ public abstract class SimpleCommand<T extends CommandSender> extends Command<T> 
     private final int COMMANDS_PER_PAGE = 9;
 
     private LinkedList<SubCommand<? extends CommandSender>> subCommands = new LinkedList<>();
+    private LinkedList<ShowcaseCommand> showcaseCommands = new LinkedList<>();
 
     public SimpleCommand(JavaPlugin plugin, String command, String permission, boolean isConsole) {
         super(plugin, permission, isConsole);
@@ -50,6 +49,10 @@ public abstract class SimpleCommand<T extends CommandSender> extends Command<T> 
         this.subCommands = subCommands;
     }
 
+    public void addShowcaseCommand(String name, String description) {
+        showcaseCommands.add(new ShowcaseCommand(name, description));
+    }
+
     protected void setSubCommands(SubCommand<? extends CommandSender>... subCommands) {
         this.subCommands.addAll(Arrays.asList(subCommands));
     }
@@ -61,21 +64,34 @@ public abstract class SimpleCommand<T extends CommandSender> extends Command<T> 
     }
 
     public void sendHelpPage(CommandSender sendTo, String color, String[] args) {
-        if (pageCount == null) pageCount = (int) Math.ceil((float) this.subCommands.size() / COMMANDS_PER_PAGE);
-
         int page = (args.length == 0 ? 0 : StringUtils.isNumeric(args[0]) ? Math.max(0, Integer.parseInt(args[0])) : 1) - 1;
+
+        int subCount = subCommands.size() + (page == 0 ? showcaseCommands.size() : 0);
+        if (pageCount == null) pageCount = (int) Math.ceil((float) (subCount) / COMMANDS_PER_PAGE);
         page = Math.min(Math.max(0, page), pageCount);
 
         PluginDescriptionFile description = super.plugin.getDescription();
         Text.sendMessage(sendTo, color + "[<] &8+-------< " + color + "&l" + description.getName().concat(" &7Page " + (page + 1) + "/" + pageCount) + " &8>-------+ " + color + "[>]");
-
         Text.sendMessage(sendTo, " ");
-        for (SubCommand s : subCommands.subList(page * COMMANDS_PER_PAGE, Math.min(subCommands.size(), (page + 1) * COMMANDS_PER_PAGE))) {
+
+        if (page == 0 && !showcaseCommands.isEmpty()) {
+            for (ShowcaseCommand s : showcaseCommands) {
+                Text.sendMessage(sendTo, "  /" + s.name + " &8-&e " + s.description);
+            }
+        }
+
+        for (SubCommand s : subCommands.subList(page * COMMANDS_PER_PAGE, Math.min(subCount, (page + 1) * COMMANDS_PER_PAGE))) {
             Text.sendMessage(sendTo, "  " + s.getFormatted(command));
         }
         Text.sendMessage(sendTo, " ");
         Text.sendMessage(sendTo, "  &2<> &f- Required Arguments&7; &9[] &f- Optional Arguments");
 
         Text.sendMessage(sendTo, color + "[<] &8+-------< " + color + "&l" + description.getName().concat(" &7v" + description.getVersion() + " &8>-------+ " + color + "[>]"));
+    }
+
+    @AllArgsConstructor
+    class ShowcaseCommand {
+        private String name;
+        private String description;
     }
 }
