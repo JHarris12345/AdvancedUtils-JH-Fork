8f6ffdaa47e6e2f7b93b0ab2f2c5b136cfc91667
GC-spigot <lietuvis.travian@gmail.com>
2024-02-07 20:29:55 +0100

    Added ImmutablePair

---
+++ src/main/java/net/advancedplugins/utils/tuple/ImmutablePair.java
@@ -0,0 +1,28 @@
+package net.advancedplugins.utils.tuple;
+
+import javax.annotation.concurrent.Immutable;
+import javax.annotation.concurrent.ThreadSafe;
+
+@Immutable
+@ThreadSafe
+public class ImmutablePair<K, V> {
+    private final K key;
+    private final V value;
+
+    public ImmutablePair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public static <S, U> ImmutablePair<S, U> of(S key, U value) {
+        return new ImmutablePair<>(key, value);
+    }
+
+    public K getKey() {
+        return this.key;
+    }
+
+    public V getValue() {
+        return this.value;
+    }
+}
