b2936c1e7947d863ad5b496405e3b106cba60b12
GC <lietuvis.travian@gmail.com>
2024-03-02 10:42:29 +0200

    updated anti tamper timeout

--- src/main/java/net/advancedplugins/utils/ASManager.java
+++ src/main/java/net/advancedplugins/utils/ASManager.java
@@ -89,8 +89,8 @@ public class ASManager {
                 Date pluginYmlLastModified = new Date(zipFile.getEntry(pluginYmlPath).getTime());
 
                 // Check modification times
-                if (Math.abs(registryLastModified.getTime() - pluginYmlLastModified.getTime()) > 30000 ||
-                        Math.abs(coreLastModified.getTime() - pluginYmlLastModified.getTime()) > 30000 ||
+                if (Math.abs(registryLastModified.getTime() - pluginYmlLastModified.getTime()) > 60000 ||
+                        Math.abs(coreLastModified.getTime() - pluginYmlLastModified.getTime()) > 60000 ||
                         coreLastModified.getYear() < 124) {
 //                    ASManager.getInstance().getLogger().severe(coreLastModified.getYear() + " Either Registry.class or Core.class has " +
 //                            "been modified significantly after plugin.yml " + registryLastModified.getTime() + " " + coreLastModified.getTime() + " " + pluginYmlLastModified.getTime());
