0b3c247d6de27d649cdee498e5bcb94ad73b8c19
KPGTB <kpg2k6@gmail.com>
2024-06-09 17:26:35 +0200

    refactor

--- src/main/java/net/advancedplugins/utils/ASManager.java
+++ src/main/java/net/advancedplugins/utils/ASManager.java
@@ -39,7 +39,6 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
-import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
