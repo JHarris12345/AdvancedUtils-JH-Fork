e276436a40de85ee0b7bccc8d06d872a676c0601
GC <lietuvis.travian@gmail.com>
2024-11-25 10:41:15 +0100

    Maybe fixed griefdef integration

--- src/main/java/net/advancedplugins/utils/protection/external/GriefDefenderCheck.java
+++ src/main/java/net/advancedplugins/utils/protection/external/GriefDefenderCheck.java
@@ -1,10 +1,15 @@
 package net.advancedplugins.utils.protection.external;
 
 import com.griefdefender.api.GriefDefender;
+import com.griefdefender.api.Subject;
+import com.griefdefender.api.Tristate;
 import com.griefdefender.api.claim.Claim;
+import com.griefdefender.api.permission.flag.Flags;
 import net.advancedplugins.utils.protection.ProtectionType;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+
+import java.util.Collections;
 
 public class GriefDefenderCheck implements ProtectionType {
     @Override
@@ -12,14 +17,30 @@ public class GriefDefenderCheck implements ProtectionType {
         return "GriefDefender";
     }
 
-    @Override
-    public boolean canBreak(Player p, Location b) {
-        final Claim claim = GriefDefender.getCore().getClaimAt(b);
+    public boolean canBreak(Player player, Location location) {
+        // Get the claim at the specified location
+        Claim claim = GriefDefender.getCore().getClaimAt(location);
 
-        if (claim != null && !claim.isWilderness()) {
-            return claim.getPlayers().contains(p.getUniqueId());
+        // If there is no claim or it's wilderness, allow breaking
+        if (claim == null || claim.isWilderness()) {
+            return true;
         }
-        return true;
+
+        // Get the Subject representing the player
+        Subject subject = GriefDefender.getCore().getUser(player.getUniqueId());
+
+        // Check if the player has permission to break blocks in this claim
+        Tristate result = GriefDefender.getPermissionManager().getActiveFlagPermissionValue(
+                claim,
+                subject,
+                Flags.BLOCK_BREAK,
+                null, // Source object (can be null for general checks)
+                null, // Target object (can be null for general checks)
+                Collections.emptySet() // No additional contexts
+        );
+
+        // Return true if permission is explicitly allowed, false otherwise
+        return result == Tristate.TRUE;
     }
 
     public boolean isProtected(Location loc) {
