a077efc04398a8eb0595b454fe1bc32c3b8b523e
Tomáš "Wega" Weglarz <82312488+ThomasWega@users.noreply.github.com>
2025-01-10 16:01:25 +0100

    Fixed %has enchantment in ....% placeholders parsing only when enchantments are present on the item

--- src/main/java/net/advancedplugins/utils/hooks/plugins/AdvancedEnchantmentsHook.java
+++ src/main/java/net/advancedplugins/utils/hooks/plugins/AdvancedEnchantmentsHook.java
@@ -1,10 +1,12 @@
 package net.advancedplugins.utils.hooks.plugins;
 
 import net.advancedplugins.ae.api.AEAPI;
+import net.advancedplugins.ae.enchanthandler.enchantments.AdvancedEnchantment;
 import net.advancedplugins.utils.hooks.PluginHookInstance;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class AdvancedEnchantmentsHook extends PluginHookInstance {
 
@@ -20,5 +22,13 @@ public class AdvancedEnchantmentsHook extends PluginHookInstance {
 
     public HashMap<String, Integer> getEnchantmentsOnItem(ItemStack item) {
         return AEAPI.getEnchantmentsOnItem(item);
+    }
+
+    public List<String> getAllEnchantments() {
+        return AEAPI.getAllEnchantments();
+    }
+
+    public AdvancedEnchantment getInstance(String enchantment) {
+        return AEAPI.getEnchantmentInstance(enchantment);
     }
 }
