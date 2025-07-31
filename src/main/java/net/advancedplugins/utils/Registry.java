06107eb5501869e005a702c8e15b2e7b92b6de2f
KPGTB <kok.krzysztof@outlook.com>
2025-07-31 14:58:03 +0200

    Bring back old registry key

--- src/main/java/net/advancedplugins/utils/Registry.java
+++ src/main/java/net/advancedplugins/utils/Registry.java
@@ -1,8 +1,9 @@
 package net.advancedplugins.utils;
+
 public class Registry {
 
-    public static String get() {
-        return "8888";
+	public static String get() {
+		return "9454";
 //        return "%%__USER__%%";
-    }
+	}
 }
